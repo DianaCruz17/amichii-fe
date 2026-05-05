024bd8df13a297f7f04415e3463f5a7c3e0a5618
DianaCruz17 <catnip.diana@gmail.com>
2026-05-05 16:27:31 -0600

    :sparkles: "carrousel"

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import FriendsProvider from './context/friends-context.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <FriendsProvider>
+      <App />
+    </FriendsProvider>
   </StrictMode>,
-)
+);
